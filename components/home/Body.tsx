ba2a296597c96ddd96a7a2db4274f3aac3090fdd
WalobwaD <walobwadan@gmail.com>
2023-01-10 09:18:32 +0300

    SideNav

---
+++ components/home/Body.tsx
@@ -0,0 +1,10 @@
+import styles from "../../styles/body/Body.module.scss";
+const Body = ()=> {
+    return (
+        <section className={styles.main}>
+            <div>Wambui</div>
+        </section>
+    )
+}
+
+export default Body;
